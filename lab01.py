39f6216cf214b850c6cf0165fa0ed577f1170204
Tarolosipher <66106632+Tarolosipher@users.noreply.github.com>
2020-05-29 14:26:48 +0900

    Add files via upload

---
+++ lab01.py
@@ -0,0 +1,44 @@
+"""Lab 1: Expressions and Control Structures"""
+
+def both_positive(a, b):
+    """Returns True if both a and b are positive.
+
+    >>> both_positive(-1, 1)
+    False
+    >>> both_positive(1, 1)
+    True
+    """
+    return a * b > 0 # You can replace this line!
+
+def sum_digits(x):
+    """Sum all the digits of x.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    sum_result = 0
+    while x > 0:
+        sum_result, x = sum_result + x % 10, x // 10
+    return sum_result 
+
+
+def falling(n,k):
+    """ Compute the falling factorial of n to depth k
+
+    >>> falling(6,3) # 6 * 5 * 3
+    120
+    >>> falling(4,3) #4 * 3 * 2
+    24
+    """
+    result = 1
+    while k > 0:
+        result, n, k = result * n, n - 1, k -1 
+    return n
+
